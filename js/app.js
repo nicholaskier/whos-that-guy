ecacf41217cfaf9180df3d97abdfda0f5ae6f885
nicholaskier <nicholaskier@gmail.com>
2020-11-16 14:10:15 -0600

    add pseudocode for what is needed in js

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+/*-------------------------------- Constants --------------------------------*/
+
+// array of questions, array of answers, array of images(?)
+ 
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+// variables for 4-5 different outcomes depending on total score at end of game
+// array of questions, array of answers, array of images(?)
+/*------------------------ Cached Element References ------------------------*/
+
+// You might choose to put your game status here
+//cache buttons, image, timer(?), score count  
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+// Clicks for start box, each answer box, submit button, reset button 
+/*-------------------------------- Functions --------------------------------*/
+
+
+// total answers
+// timer
+// submit
+// check Answer
+// restart
