1d1fddff6e63e0ea06eee3d45b8d14eec25439c1
nicholaskier <nicholaskier@gmail.com>
2020-12-02 14:30:40 -0600

    change doc title

--- js/timer.js
+++
@@ -1,9 +0,0 @@
-// let timeLeft = 60
-
-// let timer = setInterval(function(){
-//     document.getElementById('timer').textContent = timeLeft 
-//     timeLeft -= 1
-//     if (timeLeft < 0){
-//         document.getElementById('timer').textContent = ''
-//     }
-// },1000)
